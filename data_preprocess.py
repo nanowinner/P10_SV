1bc9b9c2b5653e18c405723dad6cca530fe07dda
DIMP <dimp@demant.com>
2019-06-06 05:22:43 +0200

    Cleaned up TD functions from data_preprocess

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,89 +3,11 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 from configuration import get_config
-from utils import keyword_spot
 
 config = get_config()   # get arguments from parser
 
 # downloaded dataset path
 audio_path = r'C:\VoxCeleb\vox1_dev_wav\wav' # VoxCeleb1 dataset
-# audio_path = r'C:\Users\erh\PyCharmProjects\Speaker_Verification\DataSets\Vctk\wav48'          # utterance dataset
-clean_path = r'C:\Users\erh\Documents\Deep_learning\speaker_vertification\clean_testset_wav'  # clean dataset
-noisy_path = r'C:\Users\erh\Documents\Deep_learning\speaker_vertification\noisy_testset_wav'  # noisy dataset
-
-
-def extract_noise():
-    """ Extract noise and save the spectrogram (as numpy array in config.noise_path)
-        Need: paired clean and noisy data set
-    """
-    print("start noise extraction!")
-    os.makedirs(config.noise_path, exist_ok=True)              # make folder to save noise file
-    total = len(os.listdir(clean_path))                        # total length of audio files
-    batch_frames = config.N * config.M * config.tdsv_frame     # TD-SV frame number of each batch
-    stacked_noise = []
-    stacked_len = 0
-    k = 0
-    for i, path in enumerate(os.listdir(clean_path)):
-        clean, sr = librosa.core.load(os.path.join(clean_path, path), sr=8000)  # load clean audio
-        noisy, _ = librosa.core.load(os.path.join(noisy_path, path), sr=sr)     # load noisy audio
-        noise = clean - noisy       # get noise audio by subtract clean voice from the noisy audio
-        S = librosa.core.stft(y=noise, n_fft=config.nfft,
-                              win_length=int(config.window * sr), hop_length=int(config.hop * sr))   # perform STFT
-        stacked_noise.append(S)
-        stacked_len += S.shape[1]
-
-        if i % 100 == 0:
-            print("%d processing..." % i)
-
-        if stacked_len < batch_frames:   # if noise frames is short than batch frames, then continue to stack the noise
-            continue
-
-        stacked_noise = np.concatenate(stacked_noise, axis=1)[:,:batch_frames]          # concat noise and slice
-        np.save(os.path.join(config.noise_path, "noise_%d.npy" % k), stacked_noise)   # save spectrogram as numpy file
-        print(" %dth file saved" % k, stacked_noise.shape)
-        stacked_noise = []     # reset list
-        stacked_len = 0
-        k += 1
-
-    print("noise extraction is end! %d noise files" % k)
-
-
-def save_spectrogram_tdsv():
-    """ Select text specific utterance and perform STFT with the audio file.
-        Audio spectrogram files are divided as train set and test set and saved as numpy file. 
-        Need : utterance data set (VTCK)
-    """
-    print("start text dependent utterance selection")
-    os.makedirs(config.train_path, exist_ok=True)   # make folder to save train file
-    os.makedirs(config.test_path, exist_ok=True)    # make folder to save test file
-
-    utterances_spec = []
-    for folder in os.listdir(audio_path):
-        utter_path= os.path.join(audio_path, folder, os.listdir(os.path.join(audio_path, folder))[0])
-        if os.path.splitext(os.path.basename(utter_path))[0][-3:] != '001':  # if the text utterance doesn't exist pass
-            print(os.path.basename(utter_path)[:4], "001 file doesn't exist")
-            continue
-
-        utter, sr = librosa.core.load(utter_path, config.sr)               # load the utterance audio
-        utter_trim, index = librosa.effects.trim(utter, top_db=14)         # trim the beginning and end blank
-        if utter_trim.shape[0]/sr <= config.hop*(config.tdsv_frame+2):     # if trimmed file is too short, then pass
-            print(os.path.basename(utter_path), "voice trim fail")
-            continue
-
-        S = librosa.core.stft(y=utter_trim, n_fft=config.nfft,
-                              win_length=int(config.window * sr), hop_length=int(config.hop * sr))  # perform STFT
-        S = keyword_spot(S)          # keyword spot (for now, just slice last 80 frames which contains "Call Stella")
-        utterances_spec.append(S)    # make spectrograms list
-
-    utterances_spec = np.array(utterances_spec)  # list to numpy array
-    np.random.shuffle(utterances_spec)           # shuffle spectrogram (by person)
-    total_num = utterances_spec.shape[0]
-    train_num = (total_num//10)*9                # split total data 90% train and 10% test
-    print("selection is end")
-    print("total utterances number : %d"%total_num, ", shape : ", utterances_spec.shape)
-    print("train : %d, test : %d"%(train_num, total_num- train_num))
-    np.save(os.path.join(config.train_path, "train.npy"), utterances_spec[:train_num])  # save spectrogram as numpy file
-    np.save(os.path.join(config.test_path, "test.npy"), utterances_spec[train_num:])
 
 
 def save_spectrogram_tisv():
@@ -141,7 +63,4 @@ def save_spectrogram_tisv():
 
 if __name__ == "__main__":
     # extract_noise()
-    if config.tdsv:
-        save_spectrogram_tdsv()
-    else:
-        save_spectrogram_tisv()
+    save_spectrogram_tisv()
